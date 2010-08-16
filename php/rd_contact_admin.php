edc1188f0c0abc1c5ee015a435a44e2075f41b84
paulreitz <paulreitz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-16 16:35:54 +0000

    Adding files in the PHP folder. (not added durring initial commit).
    git-svn-id: https://plugins.svn.wordpress.org/rd-contact-info/trunk@276766 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/rd_contact_admin.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Saves the current entries if the for was previously submitted, then displays the form again with the current information displayed in the appropriate fields
+ */
+if ($_POST['rd_contact_hidden'] == 'Y') // Form was previously submitted.
+	{
+		if ( function_exists( 'check_admin_referer' ) )
+			check_admin_referer( 'rdcontactinfo-editcontactinfo' );
+			
+		$rdContactName = $_POST['rdContactName'];
+		update_option('rdContactName', $rdContactName);
+		
+		$rdContactPhone = $_POST['rdContactPhone'];
+		update_option('rdContactPhone', $rdContactPhone);
+		
+		$rdContactMobile = $_POST['rdContactMobile'];
+		update_option('rdContactMobile', $rdContactMobile);
+		
+		$rdContactFax = $_POST['rdContactFax'];
+		update_option('rdContactFax', $rdContactFax);
+		
+		$rdContactEmail = $_POST['rdContactEmail'];
+		update_option('rdContactEmail', $rdContactEmail);
+		
+		$rdContactStreet = $_POST['rdContactStreet'];
+		update_option('rdContactStreet', $rdContactStreet);
+		
+		$rdContactCity = $_POST['rdContactCity'];
+		update_option('rdContactCity', $rdContactCity);
+		
+		$rdContactState = $_POST['rdContactState'];
+		update_option('rdContactState', $rdContactState);
+		
+		$rdContactZip = $_POST['rdContactZip'];
+		update_option('rdContactZip', $rdContactZip);
+		
+		$rdShowContactName = (isset($_POST['rdShowContactName']))? '1' : '0';
+		update_option('rdShowContactName', $rdShowContactName);
+		$rdShowContactName = (isset($_POST['rdShowContactName']))? 'checked' : '';
+		
+		$rdShowContactPhone = (isset($_POST['rdShowContactPhone']))? '1' : '0';
+		update_option('rdShowContactPhone', $rdShowContactPhone);
+		$rdShowContactPhone = (isset($_POST['rdShowContactPhone']))? 'checked' : '';
+		
+		$rdShowContactMobile = (isset($_POST['rdShowContactMobile']))? '1' : '0';
+		update_option('rdShowContactMobile', $rdShowContactMobile);
+		$rdShowContactMobile = (isset($_POST['rdShowContactMobile']))? 'checked' : '';
+		
+		$rdShowContactFax = (isset($_POST['rdShowContactFax']))? '1' : '0';
+		update_option('rdShowContactFax', $rdShowContactFax);
+		$rdShowContactFax = (isset($_POST['rdShowContactFax']))? 'checked' : '';
+		
+		$rdShowContactEmail = (isset($_POST['rdShowContactEmail']))? '1' : '0';
+		update_option('rdShowContactEmail', $rdShowContactEmail);
+		$rdShowContactEmail = (isset($_POST['rdShowContactEmail']))? 'checked' : '';
+		
+		$rdShowContactAddress = (isset($_POST['rdShowContactAddress']))? '1' : '0';
+		update_option('rdShowContactAddress', $rdShowContactAddress);
+		$rdShowContactAddress = (isset($_POST['rdShowContactAddress']))? 'checked' : '';
+		?>  
+         <div class="updated"><p><strong><?php _e('Contact information saved.' ); ?></strong></p></div>  
+         <?php 
+	}
+	else // First time the page is displayed.
+	{
+		$rdContactName 		= get_option('rdContactName');
+		$rdContactPhone		= get_option('rdContactPhone');
+		$rdContactMobile		= get_option('rdContactMobile');
+		$rdContactFax			= get_option('rdContactFax');
+		$rdContactEmail			= get_option('rdContactEmail');
+		$rdContactStreet		= get_option('rdContactStreet');
+		$rdContactCity			= get_option('rdContactCity');
+		$rdContactState		= get_option('rdContactState');
+		$rdContactZip			= get_option('rdContactZip');
+		
+		$rdShowContactName		= (get_option('rdShowContactName') == '1')? 'checked' : '';
+		$rdShowContactPhone		= (get_option('rdShowContactPhone') == '1')? 'checked' : '';
+		$rdShowContactMobile		= (get_option('rdShowContactMobile') == '1')? 'checked' : '';
+		$rdShowContactFax			= (get_option('rdShowContactFax') == '1')? 'checked' : '';
+		$rdShowContactEmail		= (get_option('rdShowContactEmail') == '1')? 'checked' : '';
+		$rdShowContactAddress	= (get_option('rdShowContactAddress') == '1')? 'checked' : '';
+	}
+?>
+
+<div class="wrap">
+	<?php echo '<h2>'.__('Edit Contact Information').'</h2>'; ?>
+    <form name="rd_contact_form" method="post" action="<?php echo str_replace('%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+    	<?php
+			if ( function_exists( 'wp_nonce_field' ) )
+				wp_nonce_field( 'rdcontactinfo-editcontactinfo' );
+		?>
+    	<input type="hidden" name="rd_contact_hidden" value="Y" />
+        <?php echo '<h4>'.__('Contact Information').'</h4>'; ?>
+        
+        <table>
+        	<tr>
+            	<td>
+                	<?php echo _e('Contact Name: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactName" value="<?php echo $rdContactName; ?>" size="20" />
+                </td>
+                <td>
+                	<input type="checkbox" name="rdShowContactName" <?php echo $rdShowContactName; ?>/> <?php echo _e('Show Contact Name'); ?>
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('Phone: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactPhone" value="<?php echo $rdContactPhone; ?>" size="20" />
+                </td>
+                <td>
+                	<input type="checkbox" name="rdShowContactPhone" <?php echo $rdShowContactPhone; ?>/><?php echo _e('Show Phone Number'); ?>
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('Mobile: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactMobile" value="<?php echo $rdContactMobile; ?>" size="20" />
+                </td>
+                <td>
+                	<input type="checkbox" name="rdShowContactMobile" <?php echo $rdShowContactMobile; ?>/><?php echo _e('Show Mobile Number'); ?>
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('Fax: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactFax" value="<?php echo $rdContactFax; ?>" size="20" />
+                </td>
+                <td>
+                	<input type="checkbox" name="rdShowContactFax" <?php echo $rdShowContactFax; ?>/><?php echo _e('Show Fax Number'); ?>
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('Email: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactEmail" value="<?php echo $rdContactEmail; ?>" size="20" />
+                </td>
+                <td>
+                	<input type="checkbox" name="rdShowContactEmail" <?php echo $rdShowContactEmail; ?>/><?php echo _e('Show Email Address'); ?>
+                </td>
+             </tr>
+        </table>
+        <hr />
+        <?php echo '<h4>'.__('Street Address').'</h4>' ?>
+        <input type="checkbox" name="rdShowContactAddress" <?php echo $rdShowContactAddress; ?>/><?php echo _e('Show Address'); ?>
+        <table>
+        	<tr>
+            	<td>
+                	<?php echo _e('Street Address: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactStreet" value="<?php echo $rdContactStreet; ?>" size="50" />
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('City: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactCity" value="<?php echo $rdContactCity; ?>" size="20" />
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('State: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactState" value="<?php echo $rdContactState; ?>" size="5" />
+                </td>
+            </tr>
+            <tr>
+            	<td>
+                	<?php echo _e('Zip: '); ?>
+                </td>
+                <td>
+                	<input type="text" name="rdContactZip" value="<?php echo $rdContactZip; ?>" size="15" />
+                </td>
+            </tr>
+        </table>
+        <p class="submit">
+        	<input type="submit" value="<?php _e('Save Contact Info'); ?>" />
+        </p>
+    </form>
+</div>
+<br /><br />
+<hr />
+If you find this plugin useful, please consider making a donation to its continued development. Thank you.<br />
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="PM99FQRRFCVXU">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
---
+++ php/rd_contact_shortcode.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Manages all of the shortcodes for the RD Contact Info plugin
+ */
+ 
+ /**
+  * Base shortcode that displays the contact information according 
+  * to the globla contact info settings.
+  *
+  * @param 		name-label		The label displayed in front of the contact name
+  * @param		phone-label		The label displayed in front of the phone number
+  * @param		mobile-label		The label displayed in front of the mobile number
+  * @param		fax-label			The label displayed in front of the fac number
+  * @param		email-label		The label displayed in front of the email address
+  * @param		email-link			true to add a 'mailto:' link to the email address, false to show the email without a link (default = true)
+  * @param		address-label	The label displayed *above* the address.
+  */
+ if ( !function_exists( 'rd_contact_base_shortcode' ) ):
+ function rd_contact_base_shortcode( $attr, $content )
+ {
+	 $atts = shortcode_atts(array(
+								   'name_label'			=> '',
+								   'phone_label'		=> '',
+								   'mobile_label'		=> '',
+								   'fax_label'				=> '',
+								   'email_label'			=> '',
+								   'email_link'			=> 'true'),
+							 $attr);
+	 $contact_info = '';
+	 if ( get_option( 'rdContactName' ) != '' && get_option( 'rdShowContactName' ) == '1' )
+	 {
+		$contact_info .= ( $atts['name_label'] == '' )? '' : '<strong>'.$atts['name_label'].' </strong>';
+		$contact_info .= get_option( 'rdContactName' ).'<br />';
+	 }
+	 
+	 if ( get_option( 'rdContactPhone' ) != '' && get_option( 'rdShowContactPhone' ) == '1' )
+	 {
+			$contact_info .= ( $atts['phone_label'] == '' )? '' : '<strong>'.$atts['phone_label'].' </strong>';
+			$contact_info .= get_option('rdContactPhone').'<br />';
+	 }
+	 
+	 if ( get_option( 'rdContactMobile' ) != '' && get_option( 'rdShowContactMobile' ) == '1' )
+	 {
+		 $contact_info .= ( $atts['mobile_label'] == '' )? '' : '<strong>'.$atts['mobile_label'].' </strong>';
+		 $contact_info .= get_option('rdContactMobile').'<br />';
+	 }
+	 
+	 if ( get_option( 'rdContactFax' ) != '' && get_option( 'rdShowContactFax' ) == '1' )
+	 {
+		 $contact_info .= ($atts['fax_label'] == '')? '' : '<strong>'.$atts['fax_label'].' </strong>';
+		 $contact_info .= get_option('rdContactFax').'<br />';
+	 }
+	 
+	 if ( get_option( 'rdContactEmail' ) != '' && get_option( 'rdShowContactEmail' ) == '1' )
+	 {
+		 $contact_info .= ( $atts['email_label'] == '' )? '' : '<strong>'.$atts['email_label'].' </strong>';
+		 if ( $atts['email_link'] == 'true' )
+		 {
+			 $contact_info .= '<a href="mailto:'.get_option( 'rdContactEmail' ).'">'.get_option( 'rdContactEmail' ).'</a><br />';
+		 }
+		 else
+		 {
+			 $contact_info .= get_option( 'rdContactEmail' ).'<br />';
+		 }
+	 }
+	 
+	 return $contact_info;
+ }
+ endif; // END FUNCTION rd_contact_base_shortcode
+ 
+ /*****************************************************************************
+  *The following shortcodes override the "Show" option in the main settings panel. *
+  *****************************************************************************/
+ 
+ /**
+  * Displays the contact name if a contact name was provided in the settings panel.
+  *
+  * @param		label		The label displayed in front of the contact name
+  */
+if ( !function_exists( 'rd_contact_name_shortcode' ) ):
+function rd_contact_name_shortcode( $attr, $content )
+{
+	  $atts = shortcode_atts( array( 'label' => '' ), $attr );
+	  $contact_info = '';
+	  if ( get_option( 'rdContactName' ) != '' )
+	  {
+		  $contact_info .= ( $atts['label'] == '' )? '' : '<strong>'.$atts['label'].' </strong>';
+		  $contact_info .= get_option( 'rdContactName' );
+	  }
+	  
+	  return $contact_info.'<br />';
+}
+endif; // END FUNCTION rd_contact_name_shortcode
+
+
+/********************************************************************************************/
+
+
+/**
+ * Displays the phone number if one is provided in the settings panel.
+ *
+ * @param 		label		The label displayed in front of the phone number.
+ */
+if ( !function_exists( 'rd_contact_phone_shortcode' ) ):
+function rd_contact_phone_shortcode( $attr, $content )
+{
+	 $atts = shortcode_atts( array( 'label' => '' ), $attr );
+	  $contact_info = '';
+	  if (get_option( 'rdContactPhone' ) != '' )
+	  {
+		  $contact_info .= ( $atts['label'] == '' )? '' : '<strong>'.$atts['label'].' </strong>';
+		  $contact_info .= get_option( 'rdContactPhone' );
+	  }
+	  
+	  return $contact_info.'<br />';
+}
+endif; // END FUNCTION rd_contact_phone_shortcode
+
+
+/********************************************************************************************/
+
+  
+/**
+ * Displays the mobile number if one is provided in the settings panel.
+ *
+ * @param		label		The label displayed in front of the phone number.
+ */
+if ( !function_exists( 'rd_contact_mobile_shortcode' ) ):
+function rd_contact_mobile_shortcode( $attr, $content )
+{
+	$atts = shortcode_atts( array( 'label' => '' ), $attr );
+	  $contact_info = '';
+	  if (get_option( 'rdContactMobile' ) != '' )
+	  {
+		  $contact_info .= ( $atts['label'] == '' )? '' : '<strong>'.$atts['label'].' </strong>';
+		  $contact_info .= get_option( 'rdContactMobile' );
+	  }
+	  
+	  return $contact_info.'<br />';
+}
+endif; // END FUNCTION rd_contact_mobile_shortcode
+
+
+/********************************************************************************************/
+
+
+/**
+ * Displays the fax number if one is provided in the settings panel.
+ *
+ * @param		label		The label to display in front of the fax number.
+ */
+if ( !function_exists( 'rd_contact_fax_shortcode' ) ):
+function rd_contact_fax_shortcode( $attr, $content )
+{
+	$atts = shortcode_atts( array( 'label' => '' ), $attr );
+	  $contact_info = '';
+	  if ( get_option( 'rdContactFax' ) != '' )
+	  {
+		  $contact_info .= ( $atts['label'] == '' )? '' : '<strong>'.$atts['label'].' </strong>';
+		  $contact_info .= get_option( 'rdContactFax' );
+	  }
+	  
+	  return $contact_info.'<br />';
+}
+endif; // END FUNCTION rd_contact_fax_shortcode
+
+
+/********************************************************************************************/
+
+
+/**
+ * Displays the email address if one is provided in the settings panel
+ *
+ * @param		label		The label to display in front of the email address.
+ * @param		link		true to add a "mailto:" link to the email, false to just show the address (default = true).
+ */
+if ( !function_exists( 'rd_contact_email_shortcode' ) ):
+function rd_contact_email_shortcode($attr, $content)
+{
+	$atts = shortcode_atts( array( 'label' => '', 'link' => 'true' ), $attr );
+	  $contact_info = '';
+	  if ( get_option( 'rdContactEmail' ) != '' )
+	  {
+		  $contact_info .= ( $atts['label'] == '' )? '' : '<strong>'.$atts['label'].' </strong>';
+		  if ($atts['link'] == 'true')
+		  {
+			  $contact_info .= '<a href="mailto:'.get_option( 'rdContactEmail' ).'">'.get_option( 'rdContactEmail' ).'</a>';
+		  }
+		  else
+		  {
+		  	 $contact_info .= get_option( 'rdContactEmail' );
+		  }
+	  }
+	  
+	  return $contact_info.'<br />';
+}
+endif; // END FUNCTION rd_contact_email_shortcode
+
+
+/********************************************************************************************/
+
+
+/**
+ * Displays the address provided in the settings panel.
+ *
+ * @param		label		The label displayed *above* the address.
+ */
+if ( !function_exists( 'rd_contact_address_shortcode' ) ):
+function rd_contact_address_shortcode( $attr, $content )
+{
+	$atts = shortcode_atts( array( 'label' => '' ), $attr);
+	$contact_info = '';
+	if (		get_option( 'rdContactStreet' ) != '' ||
+			get_option( 'rdContactCity' ) != '' ||
+			get_option( 'rdContactState' ) != '' ||
+			get_option( 'rdContactZip' ) != '' )
+	{
+		$contact_info .= ( $atts['label'] == '' )? '' : '<strong>'.$atts['label'].'</strong><br />';
+		$contact_info .= ( get_option( 'rdContactStreet' ) == '' )? '' : get_option( 'rdContactStreet' ).'<br />';
+		if ( get_option( 'rdContactCity' ) != '' && get_option( 'rdContactState' ) != '' )
+		{
+			$contact_info .= get_option( 'rdContactCity' ).', '.get_option( 'rdContactState' ).' ';
+		}
+		else
+		{
+			$contact_info .= ( get_option( 'rdContactCity' ) == '' )? '' : get_option( 'rdContactCity' ).' ';
+			$contact_info .= ( get_option( 'rdContactState' ) == '' )? '' : get_option( 'rdContactState' ).' ';
+		}
+		$contact_info .= ( get_option( 'rdContactZip' ) == '' )? '' : get_option( 'rdContactZip' );
+	}
+	
+	return $contact_info.'<br />';
+}
+endif; // END FUNCTION rd_contact_address_shortcode
+?>
